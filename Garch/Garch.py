9178d19da3e4561b0c84cac674457f2c678ee33c
Jhonattanln <63247251+Jhonattanln@users.noreply.github.com>
2020-07-08 23:12:46 -0300

    Update Garch.py

--- Garch/Garch.py
+++ Garch/Garch.py
@@ -1,1 +1,23 @@
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
 from arch import arch_model
+
+df = pd.read_excel(r'C:\Users\Jhona\OneDrive\Área de Trabalho\Atualizado.xlsx', index_col='Data')
+
+returns = df['CPLE6'].pct_change()*100
+
+#####Especificar o modelo garch
+basic_gm = arch_model(returns[1:], p=1, q=1, mean='constant', vol='GARCH', dist='normal')
+gm_result = basic_gm.fit()
+
+####Display model
+print(gm_result.summary())
+
+plt.style.use('ggplot')
+gm_result.plot()
+plt.show()
+
+####Estimando volatilidade 
+gm_forecast = gm_result.forecast(horizon = 5)
+print(gm_forecast.variance[-1:])
